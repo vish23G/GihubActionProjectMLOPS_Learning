a8845645f44e1ad1db53a983f5ca6224ea2ba2ec
vish23G <126935001+vish23G@users.noreply.github.com>
2025-11-25 23:31:54 +0530

    Add test cases for add and subtract function

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -1,1 +1,16 @@
+from src.math_opearions import add,sub
 
+## Add Test Case
+def test_add():
+  assert add(2,3)==5
+  assert add(-1,1)==0
+  assert add(6,4)==10
+
+## Substract Test Case
+
+def test_sub():
+  assert sub(3,4)==-1
+  assert sub(5,3)==2
+  assert sub(2,2)==0
+  
+  
