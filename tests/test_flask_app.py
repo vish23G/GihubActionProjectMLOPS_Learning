d880898a07e867b221de2a25c9cfec75659ba383
vish23G <126935001+vish23G@users.noreply.github.com>
2025-11-27 23:47:24 +0530

    Update test_flask_app.py

--- tests/test_flask_app.py
+++ tests/test_flask_app.py
@@ -5,4 +5,4 @@ def test_home():
   ## app from falsk app, test_client is funcation, get is for home page come
   responce=app.test_client().get("/")
   assert responce.status_code==200
-  assert responce.data= b"Hello word!!, Test case"
+  assert responce.data= b"Hello Word, This is Flask application!!""
