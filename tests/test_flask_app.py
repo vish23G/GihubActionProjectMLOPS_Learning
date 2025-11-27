f61e64c26c575d02afcec7ff322ccd4a7769acb2
vish23G <126935001+vish23G@users.noreply.github.com>
2025-11-27 22:56:42 +0530

    Update home route test in test_flask_app.py
    Fix test case for home route in Flask app

--- tests/test_flask_app.py
+++ tests/test_flask_app.py
@@ -1,1 +1,8 @@
+from src.flask_app import flask_app
 
+
+def test_home():
+  ## app from falsk app, test_client is funcation, get is for home page come
+  responce=app.test_client().get("/")
+  assert responce.status_code==200
+  assert responce.data= b"Hello word!!, Test case"
