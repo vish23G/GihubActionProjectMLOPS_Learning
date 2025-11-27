7837c2c8d9eddf29abb5d3e71f86b54f4ac53a51
vish23G <126935001+vish23G@users.noreply.github.com>
2025-11-27 22:44:19 +0530

    Initialize Flask application with a home route

--- src/flask_app.py
+++ src/flask_app.py
@@ -1,1 +1,11 @@
+from flask import Flask
+
+app=Flask(__name__)
+
+@app.route("/")
+def home():
+  return "Hello Word, This is Flask application!!"
+
+if __name == "__main__":
+  app.run(host="0.0.0.0",port=5000)
 
